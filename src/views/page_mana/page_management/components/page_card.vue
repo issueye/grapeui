d3051471406d0c3809290fa9cbb0eabf52afc7f1
issueye <issueye@yeah.net>
2024-05-19 21:03:08 +0800

    更新

--- src/views/page_mana/page_management/components/page_card.vue
+++ src/views/page_mana/page_management/components/page_card.vue
@@ -1,5 +1,10 @@
 <template>
-  <el-card>
+  <el-card style="transform: rotate(360deg)">
+    <el-icon
+      style="position: fixed; top: 5px; right: 5px; z-index: 10"
+      @click="onDeleteClick"
+      ><Close
+    /></el-icon>
     <div style="display: flex; flex-direction: column; height: 100%">
       <el-image :src="data.imgUrl || defaultImg" fit="cover" />
       <div class="body">
@@ -10,17 +15,28 @@
           版本：{{ data.version ? data.version : "无" }}
         </div>
       </div>
+      <el-divider />
       <div class="actions">
-        <el-button color="#2376b7" size="small" :icon="List" @click="onEditClick" />
-        <el-button color="#626aef" size="small" :icon="Edit" @click="onEditClick" />
-        <el-button color="#ee3f4d" size="small" :icon="Delete" @click="onDeleteClick" />
+        <el-button circle color="#626aef" :icon="Edit" @click="onEditClick" />
+        <el-button
+          circle
+          color="#ee3f4d"
+          :icon="VideoPause"
+          @click="onStateClick"
+        />
+        <!-- <el-button
+          color="#ee3f4d"
+          size="small"
+          :icon="Delete"
+          @click="onDeleteClick"
+        /> -->
       </div>
     </div>
   </el-card>
 </template>
 
 <script setup>
-import { Delete, Edit, List } from "@element-plus/icons-vue";
+import { Edit, VideoPlay, VideoPause } from "@element-plus/icons-vue";
 import { toRefs } from "vue";
 import defaultImg from "@/assets/images/default.png";
 
@@ -41,13 +57,18 @@ console.log("data", data);
 
 const emits = defineEmits(["edit", "delete"]);
 
+// 修改信息
 const onEditClick = () => {
   emits("edit", props.data);
 };
 
+// 删除
 const onDeleteClick = () => {
   emits("delete", props.data);
 };
+
+// 修改状态
+const onStateClick = () => {};
 </script>
 
 <style lang="scss" scoped>
@@ -64,32 +85,44 @@ const onDeleteClick = () => {
 
   .el-image {
     width: 100%;
-    height: 150px;
+    height: 140px;
     object-fit: none;
   }
 
   .body {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+
+    padding: 12px 16px;
+    height: calc(100% - 200px);
     .title {
-      margin: 10px 0;
       font-size: 16px;
       font-weight: 500;
     }
 
     .version {
-      margin-top: 10px;
+      margin-top: 8px;
       font-size: 14px;
       color: #bcbcbc;
     }
   }
 
   .actions {
-    margin-top: 15px;
     display: flex;
     justify-content: flex-end;
+    margin: 8px 12px;
+    height: 32px;
   }
 }
 
 ::v-deep(.el-card__body) {
-  padding: 10px;
+  padding: 0px;
+  height: 100%;
+}
+
+::v-deep(.el-divider--horizontal) {
+  margin: 0px;
+  border-top: 1px solid #f0f2f5;
 }
 </style>
